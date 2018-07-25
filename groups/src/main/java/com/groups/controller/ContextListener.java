27a1ee4de40260ce34eb5fcade65f7b7c25e8939
rajashekar-24 <singarapurajashekar@gmail.com>
2018-07-25 16:18:20 +0530

    Adding Groups Application files

---
+++ groups/src/main/java/com/groups/controller/ContextListener.java
@@ -0,0 +1,28 @@
+package com.groups.controller;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.io.File;
+
+@WebListener("application context listener")
+public class ContextListener implements ServletContextListener {
+    /**
+     * Initialize log4j when the application is being started
+     */
+    public void contextInitialized(ServletContextEvent event) {
+        // initialize log4j here
+        ServletContext context = event.getServletContext();
+        String log4jConfigFile = ((ServletContext) context).getInitParameter("log4j-config-location");
+        String fullPath = ((ServletContext) context).getRealPath("") + File.separator + log4jConfigFile;
+        PropertyConfigurator.configure(fullPath);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+        // do nothing
+    }
+}
---
+++ groups/src/main/java/com/groups/service/GroupServiceImp.java
@@ -0,0 +1,66 @@
+package com.groups.service;
+
+import com.groups.dao.GroupDao;
+import com.groups.dao.GroupDaoImp;
+import com.groups.model.Group;
+import org.apache.log4j.Logger;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * GroupServiceImp implements Groupservice interface, it has CreateGroupService, RetrieveGroupDetails, deleteGroupService methods
+ */
+public class GroupServiceImp implements GroupService {
+    Logger log = Logger.getLogger(GroupServiceImp.class);
+    GroupDao groupDao = new GroupDaoImp();
+    int status = 0;
+
+    /**
+     * CreateGroupService accepts the groupObject from the controller and passes it to the CreateDao and get the status.
+     *
+     * @param groupObj
+     * @return
+     * @throws SQLException
+     */
+    @Override
+    public int CreateGroupService(Group groupObj) throws SQLException {
+        status = groupDao.CreateGroupDao(groupObj);
+        log.info(status);
+        return status;
+    }
+
+    /**
+     * RetrieveGroupDetails accepts the groupName from the controller and passes it to the RetrieveGroupDetails DAO method and get the list
+     *
+     * @param groupName
+     * @return
+     * @throws SQLException
+     */
+    @Override
+    public List<Group> RetrieveGroupDetails(String groupName) throws SQLException {
+        List<Group> list = null;
+        try {
+            list = groupDao.RetrieveGroupDetails(groupName);
+            log.info(list);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("retrievegroupdetails", e);
+
+        }
+        return list;
+    }
+
+    /**
+     * deleteGroupService accepts the array of gid and passes it to the deleteGroupDao and get the status and return it to controller.
+     *
+     * @param gid
+     * @return
+     */
+    @Override
+    public int deleteGroupService(String[] gid) {
+        status = groupDao.deleteGroupDao(gid);
+        log.info(status);
+        return status;
+    }
+}
